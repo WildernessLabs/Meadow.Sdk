2c9b4a86bae9eeaeeef63e2bbbf7e9a62e987974
adrianstevens <adrian.stevens@gmail.com>
2019-12-23 13:30:11 -0800

    Update template and bump version to 0.3.8

--- Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplication/MeadowApp.cs
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplication/MeadowApp.cs
@@ -7,37 +7,55 @@ using Meadow.Foundation.Leds;
 
 namespace MeadowApp
 {
-    public class MeadowApp : App<F7Micro, MeadowApp>
-{
-	const int pulseDuration = 3000;
-	RgbPwmLed rgbPwmLed;
-
-	public MeadowApp()
+	public class MeadowApp : App<F7Micro, MeadowApp>
 	{
-		rgbPwmLed = new RgbPwmLed(Device,
-				   Device.Pins.OnboardLedRed,
-				   Device.Pins.OnboardLedGreen,
-				   Device.Pins.OnboardLedBlue);
+		RgbPwmLed onboardLed;
 
-		PulseRgbPwmLed();
-	}
-
-	protected void PulseRgbPwmLed()
-	{
-		while (true)
+		public MeadowApp()
 		{
-			Pulse(Color.Red);
-			Pulse(Color.Green);
-			Pulse(Color.Blue);
+			Initialize();
+			CycleColors(400);
+		}
+
+		void Initialize()
+		{
+			Console.WriteLine("Initialize hardware...");
+
+			onboardLed = new RgbPwmLed(device: Device,
+				redPwmPin: Device.Pins.OnboardLedRed,
+				greenPwmPin: Device.Pins.OnboardLedGreen,
+				bluePwmPin: Device.Pins.OnboardLedBlue,
+				3.3f, 3.3f, 3.3f,
+				Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+		}
+
+		void CycleColors(int duration)
+		{
+			Console.WriteLine("Cycle colors...");
+
+			while (true)
+			{
+				ShowColor(Color.Blue, duration);
+				ShowColor(Color.Cyan, duration);
+				ShowColor(Color.Green, duration);
+				ShowColor(Color.GreenYellow, duration);
+				ShowColor(Color.Yellow, duration);
+				ShowColor(Color.Orange, duration);
+				ShowColor(Color.OrangeRed, duration);
+				ShowColor(Color.Red, duration);
+				ShowColor(Color.MediumVioletRed, duration);
+				ShowColor(Color.Purple, duration);
+				ShowColor(Color.Magenta, duration);
+				ShowColor(Color.Pink, duration);
+			}
+		}
+
+		void ShowColor(Color color, int duration = 1000)
+		{
+			Console.WriteLine($"Color: {color}");
+			onboardLed.SetColor(color);
+			Thread.Sleep(duration);
+			onboardLed.Stop();
 		}
 	}
-
-	protected void Pulse(Color color)
-	{
-		rgbPwmLed.StartPulse(color);
-		Console.WriteLine($"Pulsing {color}");
-		Thread.Sleep(pulseDuration);
-		rgbPwmLed.Stop();
-	}
-}
 }
