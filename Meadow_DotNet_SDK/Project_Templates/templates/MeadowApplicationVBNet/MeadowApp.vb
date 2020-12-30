7d0c9bc6c8ffd020db4df25a96f0ade32389a503
Adrian Stevens <adrian.stevens@gmail.com>
2020-12-29 18:43:58 -0800

    Added VBNet App template

---
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplicationVBNet/MeadowApp.vb
@@ -0,0 +1,64 @@
+﻿Imports System
+Imports System.Threading
+Imports Meadow
+Imports Meadow.Devices
+Imports Meadow.Hardware
+Imports Meadow.Foundation
+Imports Meadow.Foundation.Leds
+
+Public Class MeadowApp
+
+	Inherits App(Of F7Micro, MeadowApp)
+
+	private Dim onboardLed as RgbPwmLed
+
+	Public Sub New()
+		MyBase.New
+
+		Console.WriteLine("Hello VB.NET!")
+
+		Initialize()
+		CycleColors(1000)
+
+	End Sub
+
+	Private Sub Initialize()
+		Console.WriteLine("Initialize hardware...")
+
+		onboardLed = new RgbPwmLed (Device,
+			Device.Pins.OnboardLedRed,
+			Device.Pins.OnboardLedGreen,
+			Device.Pins.OnboardLedBlue,
+			3.3f, 3.3f, 3.3f,
+			Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode)
+
+		
+
+	End Sub
+
+	Private Sub CycleColors(ByVal duration As Integer)
+		Console.WriteLine("Cycle colors...")
+
+		While true
+			ShowColorPulse(Color.Blue, duration)
+            ShowColorPulse(Color.Cyan, duration)
+            ShowColorPulse(Color.Green, duration)
+            ShowColorPulse(Color.GreenYellow, duration)
+            ShowColorPulse(Color.Yellow, duration)
+            ShowColorPulse(Color.Orange, duration)
+            ShowColorPulse(Color.OrangeRed, duration)
+            ShowColorPulse(Color.Red, duration)
+            ShowColorPulse(Color.MediumVioletRed, duration)
+            ShowColorPulse(Color.Purple, duration)
+            ShowColorPulse(Color.Magenta, duration)
+            ShowColorPulse(Color.Pink, duration)
+		End While
+
+	End Sub
+
+	Private Sub ShowColorPulse(ByVal color As Color, ByVal duration As Integer)
+		onboardLed.StartPulse(color, duration/2)
+		Thread.Sleep(duration)
+	End Sub
+
+End Class
---
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplicationVBNet/Program.vb
@@ -0,0 +1,18 @@
+﻿
+Option Explicit On
+Option Strict On
+
+Imports System
+Imports System.Threading
+
+
+Public Class Program
+	
+	Public Shared Sub Main()
+		Console.WriteLine("Hello very modern world!")
+
+		Dim app as New MeadowApp
+
+		Thread.Sleep(Timeout.Infinite)
+	End Sub
+End Class
