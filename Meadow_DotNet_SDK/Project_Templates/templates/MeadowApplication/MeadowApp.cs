83d9b91b3dc7333aea100bc6e40866514a23e031
Adrian Stevens <adrian.stevens@gmail.com>
2022-07-26 13:36:25 -0700

    Fix C# app template indentation

--- Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplication/MeadowApp.cs
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplication/MeadowApp.cs
@@ -11,63 +11,63 @@ namespace MeadowApp
 {
 	// Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
-{
-	RgbPwmLed onboardLed;
-
-	public override Task Run()
 	{
-		Console.WriteLine("Run...");
+		RgbPwmLed onboardLed;
 
-		CycleColors(TimeSpan.FromMilliseconds(1000));
-		return base.Run();
-	}
-
-	public override Task Initialize()
-	{
-		Console.WriteLine("Initialize...");
-
-		onboardLed = new RgbPwmLed(device: Device,
-			redPwmPin: Device.Pins.OnboardLedRed,
-			greenPwmPin: Device.Pins.OnboardLedGreen,
-			bluePwmPin: Device.Pins.OnboardLedBlue,
-			CommonType.CommonAnode);
-
-		return base.Initialize();
-	}
-
-	void CycleColors(TimeSpan duration)
-	{
-		Console.WriteLine("Cycle colors...");
-
-		while (true)
+		public override Task Run()
 		{
-			ShowColorPulse(Color.Blue, duration);
-			ShowColorPulse(Color.Cyan, duration);
-			ShowColorPulse(Color.Green, duration);
-			ShowColorPulse(Color.GreenYellow, duration);
-			ShowColorPulse(Color.Yellow, duration);
-			ShowColorPulse(Color.Orange, duration);
-			ShowColorPulse(Color.OrangeRed, duration);
-			ShowColorPulse(Color.Red, duration);
-			ShowColorPulse(Color.MediumVioletRed, duration);
-			ShowColorPulse(Color.Purple, duration);
-			ShowColorPulse(Color.Magenta, duration);
-			ShowColorPulse(Color.Pink, duration);
+			Console.WriteLine("Run...");
+
+			CycleColors(TimeSpan.FromMilliseconds(1000));
+			return base.Run();
+		}
+
+		public override Task Initialize()
+		{
+			Console.WriteLine("Initialize...");
+
+			onboardLed = new RgbPwmLed(device: Device,
+				redPwmPin: Device.Pins.OnboardLedRed,
+				greenPwmPin: Device.Pins.OnboardLedGreen,
+				bluePwmPin: Device.Pins.OnboardLedBlue,
+				CommonType.CommonAnode);
+
+			return base.Initialize();
+		}
+
+		void CycleColors(TimeSpan duration)
+		{
+			Console.WriteLine("Cycle colors...");
+
+			while (true)
+			{
+				ShowColorPulse(Color.Blue, duration);
+				ShowColorPulse(Color.Cyan, duration);
+				ShowColorPulse(Color.Green, duration);
+				ShowColorPulse(Color.GreenYellow, duration);
+				ShowColorPulse(Color.Yellow, duration);
+				ShowColorPulse(Color.Orange, duration);
+				ShowColorPulse(Color.OrangeRed, duration);
+				ShowColorPulse(Color.Red, duration);
+				ShowColorPulse(Color.MediumVioletRed, duration);
+				ShowColorPulse(Color.Purple, duration);
+				ShowColorPulse(Color.Magenta, duration);
+				ShowColorPulse(Color.Pink, duration);
+			}
+		}
+
+		void ShowColorPulse(Color color, TimeSpan duration)
+		{
+			onboardLed.StartPulse(color, duration / 2);
+			Thread.Sleep(duration);
+			onboardLed.Stop();
+		}
+
+		void ShowColor(Color color, TimeSpan duration)
+		{
+			onboardLed.SetColor(color);
+			Thread.Sleep(duration);
+			onboardLed.Stop();
 		}
 	}
-
-	void ShowColorPulse(Color color, TimeSpan duration)
-	{
-		onboardLed.StartPulse(color, duration / 2);
-		Thread.Sleep(duration);
-		onboardLed.Stop();
-	}
-
-	void ShowColor(Color color, TimeSpan duration)
-	{
-		onboardLed.SetColor(color);
-		Thread.Sleep(duration);
-		onboardLed.Stop();
-	}
-}
 }
