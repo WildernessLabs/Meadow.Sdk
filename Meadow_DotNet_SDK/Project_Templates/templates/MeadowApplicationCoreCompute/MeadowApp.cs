7777f6706b555fecbdd1501c47e29b67cd813c97
Adam Patridge <patridge@gmail.com>
2022-12-15 10:13:11 -0700

    Core-Compute C# template

---
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowApplicationCoreCompute/MeadowApp.cs
@@ -0,0 +1,29 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Peripherals.Leds;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MeadowApp
+{
+	// Change F7CoreComputeV2 to F7FeatherV2 (or F7FeatherV1) for Feather boards
+	public class MeadowApp : App<F7CoreComputeV2>
+	{
+		public override Task Run()
+		{
+			Resolver.Log.Info("Run...");
+
+			return base.Run();
+		}
+
+		public override Task Initialize()
+		{
+			Resolver.Log.Info("Initialize...");
+
+			return base.Initialize();
+		}
+	}
+}
