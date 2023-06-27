64b7be24ff49a574ed9bcbb337d44a5ff2c77ce7
redth <jondick@gmail.com>
2023-06-27 12:05:04 -0400

    Start laying out initial msbuild tasks
    For linking, deploying, debug startup...

---
+++ Meadow_DotNet_SDK/Meadow.SDK/MeadowDeployTask.cs
@@ -0,0 +1,64 @@
+﻿using Meadow.CLI.Core.DeviceManagement;
+using Microsoft.Build.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Meadow.SDK
+{
+	public class MeadowDeployTask : Xamarin.Build.AsyncTask
+	{
+		[Required]
+		public string OutputPath { get; set; }
+
+		public string MeadowDevicePort { get; set; }
+
+		public int DebugPort { get; set; } = -1;
+
+		MSBuildLogger Logger;
+
+		public override bool Execute()
+		{
+			Logger = new MSBuildLogger(nameof(MeadowDeployTask), Log);
+
+			System.Threading.Tasks.Task.Run(async () =>
+			{
+				try
+				{
+					await ExecuteAsync();
+				}
+				catch (System.Exception ex)
+				{
+					Log.LogErrorFromException(ex);
+				}
+				finally
+				{
+					// Invoke Complete to signal you're done.
+					Complete();
+				}
+			});
+
+			return base.Execute();
+		}
+
+		async System.Threading.Tasks.Task ExecuteAsync()
+		{
+			if (string.IsNullOrEmpty(MeadowDevicePort))
+			{
+				var availableDevicePorts = await MeadowDeviceManager.GetSerialPorts() ?? new List<string>();
+
+				MeadowDevicePort = availableDevicePorts?.FirstOrDefault();
+			}
+
+			if (string.IsNullOrEmpty (MeadowDevicePort))
+			{
+				throw new System.ArgumentNullException("No Meadow Device Port specified");
+			}
+
+			var deployer = new MeadowDeployer(Logger, MeadowDevicePort, CancellationToken);
+
+			await deployer.Deploy(OutputPath, DebugPort);
+		}
+	}
+}
---
+++ Meadow_DotNet_SDK/Meadow.SDK/MeadowDeployer.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Meadow.CLI.Core;
+using Meadow.CLI.Core.DeviceManagement;
+using Meadow.CLI.Core.Devices;
+using Meadow.CLI.Core.Internals.MeadowCommunication.ReceiveClasses;
+using Microsoft.Extensions.Logging;
+
+namespace Meadow.SDK
+{
+	public class MeadowDeployer : IDisposable
+	{
+		public MeadowDeployer(ILogger logger, string serial, CancellationToken cancellationToken)
+		{
+			Logger = logger;
+			Serial = serial;
+			CancelToken = cancellationToken;
+		}
+
+		public ILogger Logger { get; private set; }
+		public string Serial { get; private set; }
+		public CancellationToken CancelToken { get; private set; }
+
+		MeadowDeviceHelper meadow = null;
+
+		public async void Dispose()
+		{
+			try
+			{
+				if (meadow != null)
+					await meadow.MonoDisable(true, CancelToken);
+			}
+			catch { }
+
+			try { meadow?.Dispose(); }
+			finally { meadow = null; }
+		}
+		public async Task<DebuggingServer> Deploy(string folder, int debugPort = -1)
+		{
+			if (meadow == null)
+			{
+				var m = await MeadowDeviceManager.GetMeadowForSerialPort(Serial, logger: Logger);
+				if (m == null)
+					throw new InvalidOperationException("Meadow device not found");
+
+				meadow = new MeadowDeviceHelper(m, Logger);
+			}
+
+			var appPathDll = Path.Combine(folder, "App.dll");
+
+			// TODO not working reliably enough for RC1, will investigate further // if (meadow.DeviceAndAppVersionsMatch(appPathDll))
+			{
+				//wrap this is a try/catch so it doesn't crash if the developer is offline
+				try
+				{
+					string osVersion = await meadow.GetOSVersion(TimeSpan.FromSeconds(30), CancelToken);
+
+					await new DownloadManager(Logger).DownloadOsBinaries(osVersion);
+				}
+				catch
+				{
+					Logger.LogInformation("OS download failed, make sure you have an active internet connection");
+				}
+
+				var isDebugging = debugPort > 1000;
+				await meadow.DeployApp(appPathDll, isDebugging, CancelToken);
+
+				// Debugger only returns when session is done
+				if (isDebugging)
+					return await meadow.StartDebuggingSession(debugPort, CancelToken);
+			}
+
+			return null;
+		}
+	}
+}
