cbe63914d70eef8dcddd04b4c35272c5a71a4be2
Chris Tacke <ctacke@gmail.com>
2023-12-15 21:25:01 -0600

    handling user edit/display mode

--- Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/InputService.cs
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/InputService.cs
@@ -4,7 +4,93 @@ namespace StartKit.Core;
 
 public class InputService
 {
+    private DisplayMode _displayMode;
+
+    public event EventHandler<DisplayMode> DisplayModeChanged;
+    public event EventHandler HeatToIncremented;
+    public event EventHandler HeatToDecremented;
+    public event EventHandler CoolToIncremented;
+    public event EventHandler CoolToDecremented;
+
     public InputService(IStartKitPlatform platform)
     {
+        if (platform.GetRightButton() is { } rb)
+        {
+            rb.Clicked += OnRightButtonClicked;
+        }
+        if (platform.GetLeftButton() is { } lb)
+        {
+            lb.Clicked += OnLeftButtonClicked;
+        }
+        if (platform.GetUpButton() is { } ub)
+        {
+            ub.Clicked += OnUpButtonClicked;
+        }
+        if (platform.GetDownButton() is { } db)
+        {
+            db.Clicked += OnDownButtonClicked;
+        }
+    }
+
+    public DisplayMode DisplayMode
+    {
+        get => _displayMode;
+        private set
+        {
+            if (value == DisplayMode) return;
+            _displayMode = value;
+
+            DisplayModeChanged?.Invoke(this, DisplayMode);
+        }
+    }
+
+    private void OnUpButtonClicked(object sender, EventArgs e)
+    {
+        switch (DisplayMode)
+        {
+            case DisplayMode.EditCoolTo:
+                CoolToIncremented?.Invoke(this, EventArgs.Empty);
+                break;
+            case DisplayMode.EditHeatTo:
+                HeatToIncremented?.Invoke(this, EventArgs.Empty);
+                break;
+        }
+    }
+
+    private void OnDownButtonClicked(object sender, EventArgs e)
+    {
+        switch (DisplayMode)
+        {
+            case DisplayMode.EditCoolTo:
+                CoolToDecremented?.Invoke(this, EventArgs.Empty);
+                break;
+            case DisplayMode.EditHeatTo:
+                HeatToDecremented?.Invoke(this, EventArgs.Empty);
+                break;
+        }
+    }
+
+    private void OnRightButtonClicked(object sender, EventArgs e)
+    {
+        var newMode = DisplayMode switch
+        {
+            DisplayMode.None => DisplayMode.EditCoolTo,
+            DisplayMode.EditCoolTo => DisplayMode.EditHeatTo,
+            _ => DisplayMode
+        };
+
+        DisplayMode = newMode;
+    }
+
+    private void OnLeftButtonClicked(object sender, EventArgs e)
+    {
+        var newMode = DisplayMode switch
+        {
+            DisplayMode.EditHeatTo => DisplayMode.EditCoolTo,
+            DisplayMode.EditCoolTo => DisplayMode.None,
+            _ => DisplayMode
+        };
+
+        DisplayMode = newMode;
     }
 }
