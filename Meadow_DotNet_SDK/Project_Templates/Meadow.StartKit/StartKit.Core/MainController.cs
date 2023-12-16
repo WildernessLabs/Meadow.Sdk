3c2d9beb85449be5973b0354d13d3a44e067cb21
Chris Tacke <ctacke@gmail.com>
2023-12-15 20:01:31 -0600

    Added basic app state machine

--- Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/MainController.cs
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/MainController.cs
@@ -5,6 +5,7 @@ namespace StartKit.Core;
 public class MainController
 {
     private IStartKitPlatform _platform;
+    private ThermostatMode _currentMode;
 
     private CloudService _cloudService;
     private ConfigurationService _configurationService;
@@ -26,24 +27,79 @@ public class MainController
 
         // create generic services
         _displayService = new DisplayService(_platform.GetDisplay());
+        _sensorService = new SensorService(platform);
 
         // retrieve platform-dependent services
         _outputService = platform.GetOutputService();
         _bluetoothService = platform.GetBluetoothService();
 
-        return Task.CompletedTask;
-    }
-
-    public Task Run()
-    {
-        /*
-        _sensorService.TemperatureChanged += () =>
+        // connect events
+        _sensorService.CurrentTemperatureChangedHandler += (s, t) =>
         {
             // update the UI
-            // change state if necessary
+            _displayService.UpdateCurrentTemperature(t);
         };
-        */
 
         return Task.CompletedTask;
     }
+
+    public async Task Run()
+    {
+        while (true)
+        {
+            // get the current temperature
+
+            switch (_currentMode)
+            {
+                case ThermostatMode.Off:
+                    // are we above the "cool to"?
+                    if (_sensorService.CurrentTemperature > _configurationService.CoolTo)
+                    {
+                        SetSystemMode(ThermostatMode.Cool);
+                    }
+                    // are we below "heat to"?
+                    else if (_sensorService.CurrentTemperature < _configurationService.HeatTo)
+                    {
+                        SetSystemMode(ThermostatMode.Heat);
+                    }
+
+                    break;
+                case ThermostatMode.Heat:
+                    // are we above "heat to" by > deadband?
+                    if (_sensorService.CurrentTemperature > (_configurationService.HeatTo + _configurationService.Deadband))
+                    {
+                        // turn off
+                        SetSystemMode(ThermostatMode.Off);
+                    }
+                    break;
+                case ThermostatMode.Cool:
+                    // are we above "cool to" by < deadband?
+                    if (_sensorService.CurrentTemperature < (_configurationService.CoolTo + _configurationService.Deadband))
+                    {
+                        // turn off
+                        SetSystemMode(ThermostatMode.Off);
+                    }
+                    break;
+            }
+
+            await Task.Delay(_configurationService.StateCheckPeriod);
+        }
+    }
+
+    private void SetSystemMode(ThermostatMode mode)
+    {
+        if (mode == _currentMode) return;
+
+        // set the output
+        _outputService.SetMode(mode);
+
+        // update the display
+        _displayService.UpdateMode(mode);
+
+        // publish the transition
+        _cloudService.RecordTransition(_currentMode, mode);
+
+        _currentMode = mode;
+    }
+
 }
--- Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/SensorService.cs
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/SensorService.cs
@@ -1,19 +1,47 @@
-﻿using StartKit.Core.Contracts;
+﻿using Meadow.Units;
+using StartKit.Core.Contracts;
 
 namespace StartKit.Core;
 
 public class SensorService
 {
+    public event EventHandler<Temperature> CurrentTemperatureChangedHandler = default!;
+
+    private Queue<Temperature> _temperatureQueue = new();
+
+    public Temperature CurrentTemperature
+    {
+        get
+        {
+            // temperature sensor accuracy is, at best, +/- 0.5
+            var mean = _temperatureQueue.Average(t => t.Celsius);
+
+            return new Temperature(
+                Math.Round(2 * mean, MidpointRounding.AwayFromZero) * 2,
+                Temperature.UnitType.Celsius);
+        }
+    }
+
     public SensorService(IStartKitPlatform platform)
     {
         if (platform.GetTemperatureSensor() is { } t)
         {
             t.Updated += OnTemperatureUpdated;
+            t.StartUpdating(TimeSpan.FromSeconds(1));
         }
     }
 
     private void OnTemperatureUpdated(object sender, Meadow.IChangeResult<Meadow.Units.Temperature> e)
     {
-        // TODO: should we raise an event?  PropertyChanged? Other?
+        // we'll deal with larger fluctuations in the room by averaging
+        _temperatureQueue.Enqueue(e.New);
+
+        while (_temperatureQueue.Count > 5)
+        {
+            // toss out the oldest data
+            _temperatureQueue.Dequeue();
+        }
+
+        CurrentTemperatureChangedHandler?.Invoke(this, CurrentTemperature);
     }
 }
