1a36106366138ef86a1133b251f65dd909200a4a
Chris Tacke <ctacke@gmail.com>
2023-12-17 12:06:06 -0600

    Adding WIndows version of StartKit

---
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Windows/Services/OutputService.cs
@@ -0,0 +1,45 @@
+﻿using Meadow.Peripherals.Relays;
+using StartKit.Core;
+
+namespace StartKit.Windows;
+
+internal class OutputService : IOutputService
+{
+    private readonly IRelay _heatRelay;
+    private readonly IRelay _coolRelay;
+
+    public OutputService()
+    {
+        _heatRelay = new RelaySimulator("HEAT")
+        {
+            IsOn = false
+        };
+        _coolRelay = new RelaySimulator("COOL")
+        {
+            IsOn = false
+        };
+    }
+
+    public Task SetMode(ThermostatMode mode)
+    {
+        switch (mode)
+        {
+            case ThermostatMode.Off:
+                _heatRelay.IsOn = false;
+                _coolRelay.IsOn = false;
+                break;
+            case ThermostatMode.Heat:
+                _heatRelay.IsOn = true;
+                _coolRelay.IsOn = false;
+                break;
+            case ThermostatMode.Cool:
+                _heatRelay.IsOn = false;
+                _coolRelay.IsOn = true;
+                break;
+        }
+
+        Console.WriteLine($"HEAT: {(_heatRelay.IsOn ? "ON" : "OFF")}  COOL: {(_coolRelay.IsOn ? "ON" : "OFF")}");
+
+        return Task.CompletedTask;
+    }
+}
