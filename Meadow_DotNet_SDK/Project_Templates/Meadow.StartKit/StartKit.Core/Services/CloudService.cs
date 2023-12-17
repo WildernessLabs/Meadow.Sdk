1c03cdaa5c8e2e6f15754207b3443e84919e91a7
Chris Tacke <ctacke@gmail.com>
2023-12-17 10:16:03 -0600

    Added command to StartKit

--- Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/CloudService.cs
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.Core/Services/CloudService.cs
@@ -1,9 +1,58 @@
-﻿namespace StartKit.Core;
+﻿using Meadow;
+using Meadow.Cloud;
+using Meadow.Units;
+
+namespace StartKit.Core;
 
 public class CloudService
 {
-    public CloudService()
+    private ICommandService _commandService;
+
+    public event EventHandler<SetPoints> NewSetpointsReceived = default!;
+
+    public CloudService(ICommandService commandService)
     {
+        _commandService = commandService;
+
+        _commandService.Subscribe<ChangeSetpointsCommand>(OnChangeSetpointsCommandReceived);
+    }
+
+    private void OnChangeSetpointsCommandReceived(ChangeSetpointsCommand command)
+    {
+        Temperature? heatTo = null;
+        Temperature? coolTo = null;
+
+        // only one set of units is allowable in a single commnd, C wins
+        if (command.HeatToC is { } heatToC)
+        {
+            heatTo = new Temperature(heatToC, Temperature.UnitType.Celsius);
+        }
+        else if (command.HeatToF is { } heatToF)
+        {
+            heatTo = new Temperature(heatToF, Temperature.UnitType.Fahrenheit);
+        }
+
+        if (command.CoolToC is { } coolToC)
+        {
+            coolTo = new Temperature(coolToC, Temperature.UnitType.Celsius);
+        }
+        else if (command.CoolToF is { } coolToF)
+        {
+            coolTo = new Temperature(coolToF, Temperature.UnitType.Fahrenheit);
+        }
+
+        if (heatTo != null || coolTo != null)
+        {
+            var setpoints = new SetPoints
+            {
+                HeatTo = heatTo,
+                CoolTo = coolTo
+            };
+
+            Resolver.Log.Info($"Command received: {setpoints}");
+
+            NewSetpointsReceived?.Invoke(this, setpoints);
+        }
     }
 
     public Task RecordTransition(ThermostatMode previousMode, ThermostatMode newMode)
