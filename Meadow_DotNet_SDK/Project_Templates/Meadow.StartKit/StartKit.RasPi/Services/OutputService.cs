2af2b95934e6bb6546eebd658f6647144a68884d
Chris Tacke <ctacke@opennetcf.com>
2023-12-22 11:51:46 -0600

    StartKit RasPi initial cut

---
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.RasPi/Services/OutputService.cs
@@ -0,0 +1,46 @@
+﻿using Meadow.Foundation.Simulation;
+using Meadow.Peripherals.Relays;
+using StartKit.Core;
+
+namespace Meadow.RasPi;
+
+internal class OutputService : IOutputService
+{
+    private readonly IRelay _heatRelay;
+    private readonly IRelay _coolRelay;
+
+    public OutputService()
+    {
+        _heatRelay = new SimulatedRelay("HEAT")
+        {
+            State = RelayState.Open
+        };
+        _coolRelay = new SimulatedRelay("COOL")
+        {
+            State = RelayState.Open
+        };
+    }
+
+    public Task SetMode(ThermostatMode mode)
+    {
+        switch (mode)
+        {
+            case ThermostatMode.Off:
+                _heatRelay.IsClosed = false;
+                _coolRelay.IsClosed = false;
+                break;
+            case ThermostatMode.Heat:
+                _heatRelay.IsClosed = true;
+                _coolRelay.IsClosed = false;
+                break;
+            case ThermostatMode.Cool:
+                _heatRelay.IsClosed = false;
+                _coolRelay.IsClosed = true;
+                break;
+        }
+
+        Console.WriteLine($"HEAT: {(_heatRelay.IsClosed ? "ON" : "OFF")}  COOL: {(_coolRelay.IsClosed ? "ON" : "OFF")}");
+
+        return Task.CompletedTask;
+    }
+}
