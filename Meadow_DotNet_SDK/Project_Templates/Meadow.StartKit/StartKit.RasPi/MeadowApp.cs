1445837bb6337ece8e08fe19bd7ed461eee8ce7f
Chris Tacke <ctacke@opennetcf.com>
2023-12-22 11:51:46 -0600

    StartKit RasPi initial cut

--- Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.RasPi/MeadowApp.cs
+++ Meadow_DotNet_SDK/Project_Templates/Meadow.StartKit/StartKit.RasPi/MeadowApp.cs
@@ -1,21 +1,35 @@
-﻿using Meadow.Pinouts;
+﻿using Meadow.Foundation.Displays;
+using Meadow.Pinouts;
+using StartKit.Core;
 
 namespace Meadow.RasPi;
 
 internal class MeadowApp : App<Linux<RaspberryPi>>
 {
+    private StartKitPlatform<RaspberryPi> _platform;
+
+    public bool SupportDisplay { get; set; } = false;
+
     private static void Main(string[] args)
     {
         MeadowOS.Start(args);
     }
 
-    public override Task Initialize()
+    public override async Task Initialize()
     {
-        return base.Initialize();
+        _platform = new StartKitPlatform<RaspberryPi>(Device, SupportDisplay);
+        var c = new MainController();
+        await c.Initialize(_platform);
+        _ = c.Run();
     }
 
     public override Task Run()
     {
+        if (_platform.GetDisplay() is GtkDisplay gtk)
+        {
+            gtk.Run();
+        }
+
         return base.Run();
     }
 }
