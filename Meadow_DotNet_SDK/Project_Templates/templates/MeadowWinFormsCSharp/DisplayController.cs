b048be141d74bf8c229707679d1dbe59e2c0b0aa
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-01-18 21:56:35 -0800

    Add WinForms template

---
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowWinFormsCSharp/DisplayController.cs
@@ -0,0 +1,31 @@
+﻿using Meadow;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.MicroLayout;
+
+namespace MeadowApp
+{
+    public class DisplayController
+    {
+        protected DisplayScreen DisplayScreen { get; set; }
+
+        public DisplayController(IGraphicsDisplay display)
+        {
+            DisplayScreen = new DisplayScreen(display)
+            {
+                BackgroundColor = Color.FromHex("14607F")
+            };
+
+            DisplayScreen.Controls.Add(new Label(
+                left: 0,
+                top: 0,
+                width: DisplayScreen.Width,
+                height: DisplayScreen.Height)
+            {
+                Text = "Hello World",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Font = new Font12x20()
+            });
+        }
+    }
+}
