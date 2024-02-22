22f5e4f0f3fa01df4b61a3131caae9c110634345
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-02-21 16:37:54 -0800

    Update Template for Meadow.Desktop

---
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowDesktopCSharp/MeadowApp.cs
@@ -0,0 +1,41 @@
+﻿using Meadow;
+using Meadow.Foundation.Displays;
+using System.Threading.Tasks;
+
+namespace MeadowApplication.Template;
+
+public class MeadowApp : App<Desktop>
+{
+    public override Task Initialize()
+    {
+        Resolver.Log.Info($"Initializing {this.GetType().Name}");
+        Resolver.Log.Info($" Platform OS is a {Device.PlatformOS.GetType().Name}");
+        Resolver.Log.Info($" Platform: {Device.Information.Platform}");
+        Resolver.Log.Info($" OS: {Device.Information.OSVersion}");
+        Resolver.Log.Info($" Model: {Device.Information.Model}");
+        Resolver.Log.Info($" Processor: {Device.Information.ProcessorType}");
+
+        var displayController = new DisplayController(Device.Display!);
+
+        return base.Initialize();
+    }
+
+    public override Task Run()
+    {
+        // NOTE: this will not return until the display is closed
+        ExecutePlatformDisplayRunner();
+
+        return Task.CompletedTask;
+    }
+
+    private void ExecutePlatformDisplayRunner()
+    {
+#if WINDOWS
+        System.Windows.Forms.Application.Run(Device.Display as System.Windows.Forms.Form);
+#endif
+        if (Device.Display is GtkDisplay gtk)
+        {
+            gtk.Run();
+        }
+    }
+}
