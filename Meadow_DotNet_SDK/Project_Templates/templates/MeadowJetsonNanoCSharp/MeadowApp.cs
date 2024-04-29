8949bde675c11f5120325d4518f7bc68e3c38972
Jorge Ramirez <ramirez.jorgea@outlook.com>
2024-04-29 00:22:23 -0700

    Indentation cleanup

--- Meadow_DotNet_SDK/Project_Templates/templates/MeadowJetsonNanoCSharp/MeadowApp.cs
+++ Meadow_DotNet_SDK/Project_Templates/templates/MeadowJetsonNanoCSharp/MeadowApp.cs
@@ -1,28 +1,27 @@
 ﻿using Meadow;
 
-namespace MeadowApplication.Template
+namespace MeadowApplication.Template;
+
+public class MeadowApp : App<JetsonNano>
 {
-    public class MeadowApp : App<JetsonNano>
+    public override Task Initialize()
     {
-        public override Task Initialize()
-        {
-            Resolver.Log.Info("Initialize...");
+        Resolver.Log.Info("Initialize...");
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        public override Task Run()
-        {
-            Resolver.Log.Info("Run...");
+    public override Task Run()
+    {
+        Resolver.Log.Info("Run...");
 
-            Resolver.Log.Info("Hello, Jetson Nano!");
+        Resolver.Log.Info("Hello, Jetson Nano!");
 
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
+    }
 
-        static async Task Main(string[] args)
-        {
-            await MeadowOS.Start(args);
-        }
+    static async Task Main(string[] args)
+    {
+        await MeadowOS.Start(args);
     }
 }
